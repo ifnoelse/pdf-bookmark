2ebd80e5030e1a378f0bd04838efb2a2ff0c186c
ifnoelse <ifnoelse@qq.com>
2017-02-27 19:15:03 +0800

    修改测试代码

--- src/test/java/test/ContentProviderTest.java
+++ src/test/java/test/ContentProviderTest.java
@@ -1,11 +1,7 @@
 package test;
 
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-import jodd.http.HttpRequest;
-import jodd.http.HttpResponse;
+import com.ifnoelse.pdf.PDFContents;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.IOException;
 
@@ -14,22 +10,7 @@ import java.io.IOException;
  */
 public class ContentProviderTest {
     public static void main(String[] args) throws ScriptException, IOException {
-//        ContentsProvider provider = new JDContentsProvider();
-//        System.out.println(provider.getContentsByUrl("https://item.jd.com/12037418.html"));
-
-
-
-
-//        Connection.Response s = Jsoup.connect("https://dx.3.cn/desc/11886254?cdn=2&callback=showdesc").execute();
-//
-        HttpRequest request = new HttpRequest();
-        HttpResponse response = HttpRequest.get("https://dx.3.cn/desc/11886254?cdn=2&callback=showdesc").send();
-
-
-        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-        ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
-        Object o = nashorn.eval("function showdesc(s){return s;};"+response.body());
-        ScriptObjectMirror so = (ScriptObjectMirror) o;
-        System.out.println(((ScriptObjectMirror) o).get("content"));
+        System.out.println(PDFContents.getContentsByUrl("http://product.china-pub.com/5044676"));
     }
+
 }
--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,13 @@
 package test;
 
+import com.ifnoelse.pdf.Bookmark;
 import com.ifnoelse.pdf.PDFContents;
 import com.ifnoelse.pdf.PDFUtil;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by ifnoelse on 2017/2/25 0025.
@@ -11,12 +15,12 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
 
-//        String lines = PDFContents.getContentsFromChinaPub("http://product.china-pub.com/4702043");
+        //获取书籍的目录信息
+        String contents = PDFContents.getContentsByUrl("http://product.china-pub.com/223565");
 
-//        PDFUtil.addBookmark(PDFUtil.generateBookmark(lines,22),"E:\\pdf\\Java EE 7权威指南 卷2.pdf","E:\\pdf\\res.pdf");
 
-
-        System.out.println(PDFContents.getContentsByUrl("http://product.china-pub.com/3663089"));
+        //为书籍添加目录
+        PDFUtil.addBookmark(PDFUtil.generateBookmark(contents, 14), "需要添加书签的pdf路径", "添加书签之后的pdf输出路径");
 
 
     }
