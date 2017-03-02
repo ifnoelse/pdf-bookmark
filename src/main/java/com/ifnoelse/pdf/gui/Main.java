85266fdf99f65a9bdf0b5b2a577044c7742ee0c2
ifnoelse <ifnoelse@qq.com>
2017-03-02 21:08:37 +0800

    添加用户界面

--- src/main/java/com/ifnoelse/pdf/gui/Main.java
+++ src/main/java/com/ifnoelse/pdf/gui/Main.java
@@ -1,5 +1,6 @@
 package com.ifnoelse.pdf.gui;
 
+import com.ifnoelse.pdf.PDFUtil;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventType;
@@ -12,54 +13,95 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 /**
  * Created by ifnoelse on 2017/3/2 0002.
  */
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("test");
-        BorderPane hBox = new BorderPane();
-        hBox.setCenter(new Button("生成目录"));
-        Button button =  new Button("选择文件");
-        FileChooser fileChooser = new FileChooser();
+        primaryStage.setTitle("pdf bookmark");
 
-        TextArea textArea = new TextArea();
+        BorderPane bottomPane = new BorderPane();
+        Button contentsGenerator = new Button("生成目录");
+        bottomPane.setCenter(contentsGenerator);
+        Button fileSelectorBtn = new Button("选择文件");
+
 
         BorderPane vBox = new BorderPane();
         TextField filePath = new TextField();
 
         filePath.setEditable(false);
-        BorderPane top = new BorderPane();
-        top.setCenter(filePath);
-        TextField offset = new TextField();
+        BorderPane topPane = new BorderPane();
+        topPane.setCenter(filePath);
 
-        offset.setPromptText("页码偏移量");
-        offset.setPrefWidth(100);
-        top.setRight(new HBox(offset,button));
-        vBox.setTop(top);
+
+        TextField pageIndexOffset = new TextField();
+        topPane.setRight(new HBox(pageIndexOffset, fileSelectorBtn));
+        vBox.setTop(topPane);
+
+        pageIndexOffset.setPromptText("页码偏移量");
+        pageIndexOffset.setPrefWidth(100);
+
+
+        TextArea textArea = new TextArea();
         textArea.setPromptText("请在此填入目录内容");
         vBox.setCenter(textArea);
-        vBox.setBottom(hBox);
-        Scene scene = new Scene(vBox,800,600);
+
+        vBox.setBottom(bottomPane);
+        Scene scene = new Scene(vBox, 600, 400);
         primaryStage.setScene(scene);
 
 
-
-        button.setOnAction(event -> {
+        fileSelectorBtn.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("pdf", "*.pdf"));
             File file = fileChooser.showOpenDialog(null);
-            System.out.println(file);
-            System.out.println(event);
-            filePath.setText(file.getPath());
+            if (file != null) {
+                filePath.setText(file.getPath());
+            }
+
+
+        });
+
+
+        pageIndexOffset.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!observable.getValue()) {
+                String offset = pageIndexOffset.getText();
+                if (offset != null && offset.length() > 0 && !offset.matches("[0-9]+")) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("页码偏移量只能为整数");
+                    alert.setTitle("错误");
+                    alert.setHeaderText("偏移量设置错误");
+                    alert.show();
+                }
+
+            }
+            System.out.println(observable.getValue());
+        });
+
+        contentsGenerator.setOnAction(event -> {
+            String srcFile = filePath.getText().replaceAll("\\\\", "/");
+            String srcFileName = srcFile.substring(srcFile.lastIndexOf("/") + 1);
+            String ext = srcFileName.substring(srcFileName.lastIndexOf("."));
+            String destFile = srcFile.substring(0, srcFile.lastIndexOf(srcFileName)) + srcFileName.substring(0, srcFileName.lastIndexOf(".")) + "_含目录" + ext;
+
+            String offset = pageIndexOffset.getText();
+            PDFUtil.addBookmark(textArea.getText(), srcFile, destFile, Integer.parseInt(offset != null && !offset.isEmpty() ? offset : "0"));
 
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setContentText("生成的文件存储在源文件目录下，文件名为：");
+
+            alert.setContentText("文件存储在" + destFile);
             alert.setTitle("通知");
             alert.setHeaderText("添加目录成功！");
             alert.show();
-
-
 
         });
         primaryStage.show();
